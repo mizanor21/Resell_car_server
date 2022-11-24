b631192d58e4a5710bd4d8d81116cc6e41633a59
mizanor21 <mizanorrahman398@gmail.com>
2022-11-24 06:55:29 +0600

    Added run function

--- index.js
+++ index.js
@@ -12,6 +12,17 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.disah5t.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+async function run() {
+    try {
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
     res.send('Recycle web node server is running!');
 })
